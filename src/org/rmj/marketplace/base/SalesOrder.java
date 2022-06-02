6948acab58e8f96304fa19025f1d333f0c416c22
unclejo <jonsabiniano03@gmail.com>
2022-06-02 11:15:59 +0800

    Add fetching and searching data in sales oder class

--- src/org/rmj/marketplace/base/SalesOrder.java
+++ src/org/rmj/marketplace/base/SalesOrder.java
@@ -1,5 +1,22 @@
 package org.rmj.marketplace.base;
 
+import com.sun.rowset.CachedRowSetImpl;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.Date;
+import javax.sql.RowSetMetaData;
+import javax.sql.rowset.CachedRowSet;
+import javax.sql.rowset.RowSetFactory;
+import javax.sql.rowset.RowSetMetaDataImpl;
+import javax.sql.rowset.RowSetProvider;
+import org.json.simple.JSONObject;
+import org.rmj.appdriver.GRider;
+import org.rmj.appdriver.MiscUtil;
+import org.rmj.appdriver.SQLUtil;
+import org.rmj.appdriver.agentfx.ui.showFXDialog;
+import org.rmj.appdriver.constants.EditMode;
+
 /**
  *
  * @author Mac
@@ -7,5 +24,389 @@ package org.rmj.marketplace.base;
  * 2022-05-02
  */
 public class SalesOrder {
+    private final String MASTER_TABLE = "sales_order_master";
+    private final String DETAIL_TABLE = "sales_order_detail";
+    
+    private final GRider p_oApp;
+    private final boolean p_bWithParent;
+    
+    private String p_sBranchCd;
+    
+    private int p_nEditMode;
+    private int p_nTranStat;
+
+    private String p_sMessage;
+    private boolean p_bWithUI = true;
+
+    private CachedRowSet p_oMaster;
+    private LTransaction p_oListener;
+   
+    public SalesOrder(GRider foApp, String fsBranchCd, boolean fbWithParent){        
+        p_oApp = foApp;
+        p_sBranchCd = fsBranchCd;
+        p_bWithParent = fbWithParent;        
+                
+        if (p_sBranchCd.isEmpty()) p_sBranchCd = p_oApp.getBranchCode();
+        
+        p_nTranStat = 0;
+        p_nEditMode = EditMode.UNKNOWN;
+    }
+    
+    public void setTranStat(int fnValue){
+        p_nTranStat = fnValue;
+    }
+   
+    public void setListener(LTransaction foValue){
+        p_oListener = foValue;
+    }
+    
+    public void setWithUI(boolean fbValue){
+        p_bWithUI = fbValue;
+    }
+    
+    public int getEditMode(){
+        return p_nEditMode;
+    }
+    
+    public boolean LoadList(String fsTransNox, boolean fbByCode) throws SQLException{
+        if (p_oApp == null){
+            p_sMessage = "Application driver is not set.";
+            return false;
+        }
+        
+        p_sMessage = "";    
+        
+        String lsSQL = "";
+        
+        if (fbByCode)
+            lsSQL = getSQ_Master()+ " HAVING sTransNox LIKE " + SQLUtil.toSQL(fsTransNox + "%");
+        else
+            lsSQL = getSQ_Master() + " HAVING sCompnyNm LIKE " + SQLUtil.toSQL(fsTransNox + "%");
+//        
+        ResultSet loRS = p_oApp.executeQuery(getSQ_Master());
+        if (MiscUtil.RecordCount(loRS) == 0){
+            MiscUtil.close(loRS);
+            p_sMessage = "No record found for the given criteria.";
+            return false;
+        }
+        
+        RowSetFactory factory = RowSetProvider.newFactory();
+        p_oMaster = factory.createCachedRowSet();
+        p_oMaster.populate(loRS);
+        MiscUtil.close(loRS);
+        
+        return true;
+    }
+    public boolean LoadOrderDetail(String fsTransNox, boolean fbByCode) throws SQLException{
+        if (p_oApp == null){
+            p_sMessage = "Application driver is not set.";
+            return false;
+        }
+        
+        p_sMessage = "";    
+        
+        String lsSQL = "";
+        lsSQL = getSQ_Detail()+ " WHERE a.sTransNox = " + SQLUtil.toSQL(fsTransNox);
+       
+        ResultSet loRS = p_oApp.executeQuery(lsSQL);
+        if (MiscUtil.RecordCount(loRS) == 0){
+            MiscUtil.close(loRS);
+            p_sMessage = "No record found for the given criteria.";
+            return false;
+        }
+        
+        RowSetFactory factory = RowSetProvider.newFactory();
+        p_oMaster = factory.createCachedRowSet();
+        p_oMaster.populate(loRS);
+        MiscUtil.close(loRS);
+        
+        return true;
+    }
+    
+    
+    
+    public Object getMaster(int fnIndex) throws SQLException{
+        if (fnIndex == 0) return null;
+        
+        p_oMaster.first();
+        return p_oMaster.getObject(fnIndex);
+    }
+    
+    public Object getMaster(String fsIndex) throws SQLException{
+        return getMaster(getColumnIndex(p_oMaster, fsIndex));
+    }
+    
+    public void setMaster(int fnIndex, Object foValue) throws SQLException{
+        if (p_nEditMode != EditMode.ADDNEW && p_nEditMode != EditMode.UPDATE) {
+            System.out.println("Invalid Edit Mode Detected.");
+            return;
+        }
+        
+        p_oMaster.first();
+        
+        switch (fnIndex){
+            case 2:
+                if (foValue instanceof Date){
+                    p_oMaster.updateDate(fnIndex, SQLUtil.toDate((Date) foValue));
+                } else
+                    p_oMaster.updateDate(fnIndex, SQLUtil.toDate(p_oApp.getServerDate()));
+                
+                p_oMaster.updateRow();
+                
+                if (p_oListener != null) p_oListener.MasterRetreive(fnIndex, p_oMaster.getString(fnIndex));
+                break;
+            case 1: 
+            case 3:
+            case 11:
+                p_oMaster.updateString(fnIndex, (String) foValue);
+                p_oMaster.updateRow();
+                if (p_oListener != null) p_oListener.MasterRetreive(fnIndex, p_oMaster.getString(fnIndex));
+                break;
+            case 4:
+            case 5:
+            case 6: 
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+                if (foValue instanceof Integer)
+                    p_oMaster.updateInt(fnIndex, (int) foValue);
+                else 
+                    p_oMaster.updateInt(fnIndex, 0);
+                
+                if (p_oListener != null) p_oListener.MasterRetreive(fnIndex, p_oMaster.getString(fnIndex));
+                break;
+        }
+    }
+     public void setMaster(String fsIndex, Object foValue) throws SQLException{
+        setMaster(getColumnIndex(p_oMaster, fsIndex), foValue);
+    }
+   
+    public String getMessage(){
+        return p_sMessage;
+    }
+    
+    public Object getDetail(int fnRow, int fnIndex) throws SQLException{
+        if (fnIndex == 0) return null;
+        
+        p_oMaster.absolute(fnRow);
+        return p_oMaster.getObject(fnIndex);
+    }
+    
+    public Object getDetail(int fnRow, String fsIndex) throws SQLException{
+        return getDetail(fnRow, getColumnIndex(p_oMaster, fsIndex));
+    }
+    
+    public int getItemCount() throws SQLException{
+        p_oMaster.last();
+        return p_oMaster.getRow();
+    }
+    
+    public void displayMasFields() throws SQLException{
+        if (p_nEditMode != EditMode.ADDNEW && p_nEditMode != EditMode.UPDATE) return;
+        
+        int lnRow = p_oMaster.getMetaData().getColumnCount();
+        
+        System.out.println("----------------------------------------");
+        System.out.println("MASTER TABLE INFO");
+        System.out.println("----------------------------------------");
+        System.out.println("Total number of columns: " + lnRow);
+        System.out.println("----------------------------------------");
+        
+        for (int lnCtr = 1; lnCtr <= lnRow; lnCtr++){
+            System.out.println("Column index: " + (lnCtr) + " --> Label: " + p_oMaster.getMetaData().getColumnLabel(lnCtr));
+            System.out.println("Column type: " + (lnCtr) + " --> " + p_oMaster.getMetaData().getColumnType(lnCtr));
+            if (p_oMaster.getMetaData().getColumnType(lnCtr) == Types.CHAR ||
+                p_oMaster.getMetaData().getColumnType(lnCtr) == Types.VARCHAR){
+                
+                System.out.println("Column index: " + (lnCtr) + " --> Size: " + p_oMaster.getMetaData().getColumnDisplaySize(lnCtr));
+            }
+        }
+        
+        System.out.println("----------------------------------------");
+        System.out.println("END: MASTER TABLE INFO");
+        System.out.println("----------------------------------------");
+    }
+    public String getSQ_Master(){
+        String lsSQL = "";
+        
+        String lsCondition = "";
+        String lsStat = String.valueOf(p_nTranStat);
+        
+        if (lsStat.length() > 1){
+            for (int lnCtr = 0; lnCtr <= lsStat.length()-1; lnCtr++){
+                lsSQL += ", " + SQLUtil.toSQL(Character.toString(lsStat.charAt(lnCtr)));
+            }
+            lsCondition = "a.cTranStat IN (" + lsSQL.substring(2) + ")";
+        } else 
+            lsCondition = "a.cTranStat = " + SQLUtil.toSQL(lsStat);
+               
+        lsSQL = "SELECT " +
+                "  a.sTransNox," +
+                "  a.dTransact, " +
+                "  a.sTermCode," +
+                "  a.nTranTotl," +
+                "  a.nVATRatex," +
+                "  a.nDiscount," +
+                "  a.nAddDiscx," +
+                "  a.nFreightx," +
+                "  a.nAmtPaidx," +
+                "  a.cTranStat," +
+                "  a.sRemarksx," +
+                "  CONCAT(b.sFrstName, ' ', b.sMiddName,' ', b.sLastName) AS sCompnyNm," +
+                "  b.sAddressx," +
+                "  c.sTownName" +
+                "  FROM " + MASTER_TABLE +" a " +
+                "LEFT JOIN Client_Master b " +
+                "	ON a.sClientID = b.sClientID " +
+                "LEFT JOIN TownCity c " + 
+                "ON b.sTownIDxx = c.sTownIDxx " + 
+                " WHERE " + lsCondition;
+        return lsSQL;
+    }
+    
+    public String getSQ_Detail(){
+        String lsSQL = "";
+        lsSQL = "SELECT " +
+                    "  a.sTransNox, " +
+                    "  d.sBarrcode xBarCodex, " +
+                    "  d.sDescript xDescript, " +
+                    "  IFNULL(e.sBrandNme, '') xBrandNme, " +
+                    "  IFNULL(f.sModelNme, '') xModelNme, " +
+                    "  IFNULL(g.sColorNme, '') xColorNme, " +
+                    "  a.nEntryNox, " +
+                    "  a.nQuantity, " +
+                    "  a.nUnitPrce, " +
+                    "  a.sReferNox, " +
+                    "  a.nIssuedxx " +
+                    "FROM sales_order_detail a " +
+                    "LEFT JOIN mp_inv_master b " +
+                    "	ON a.sStockIDx = b.sListngID " +
+                    "LEFT JOIN inv_category c " +
+                    "	ON b.sCategrID = c.sCategrID " +
+                    "LEFT JOIN  CP_Inventory d " +
+                    "	ON d.sStockIDx = a.sStockIDx " +
+                    "LEFT JOIN CP_Brand e " +
+                    "	ON d.sBrandIDx = e.sBrandIDx " +
+                    "LEFT JOIN CP_Model f " +
+                    "	ON d.sModelIDx = f.sModelIDx " +
+                    "LEFT JOIN color g " +
+                    "	ON d.sColorIDx = g.sColorIDx";
+    
+        
+        return lsSQL;
+    }
+    
+    private int getColumnIndex(CachedRowSet loRS, String fsValue) throws SQLException{
+        int lnIndex = 0;
+        int lnRow = loRS.getMetaData().getColumnCount();
+        
+        for (int lnCtr = 1; lnCtr <= lnRow; lnCtr++){
+            if (fsValue.equals(loRS.getMetaData().getColumnLabel(lnCtr))){
+                lnIndex = lnCtr;
+                break;
+            }
+        }
+        
+        return lnIndex;
+    }
+    
+    public boolean SearchTransaction(String fsValue, boolean fbByCode) throws SQLException{
+        if (p_oApp == null){
+            p_sMessage = "Application driver is not set.";
+            return false;
+        }
+        
+        p_sMessage = "";    
+        
+        String lsSQL = MiscUtil.addCondition(getSQ_Master(), "sBranchCd = " + SQLUtil.toSQL(p_sBranchCd));
+        
+        if (p_bWithUI){
+            JSONObject loJSON = showFXDialog.jsonSearch(
+                                p_oApp, 
+                                getSQ_Master(), 
+                                fsValue, 
+                                "Order No.»Customer Name»Total Amount", 
+                                "sTransNox»sCompnyNm»nTranTotl", 
+                                "sTransNox»sCompnyNm»nTranTotl", 
+                                fbByCode ? 0 : 1);
+            
+            if (loJSON != null) 
+                return OpenTransaction((String) loJSON.get("sTransNox"));
+            else {
+                p_sMessage = "No record selected.";
+                return false;
+            }
+        }
+        
+        if (fbByCode)
+            lsSQL = MiscUtil.addCondition(lsSQL, "sTransNox = " + SQLUtil.toSQL(fsValue));   
+        else {
+            if (!fsValue.isEmpty()) {
+                lsSQL = MiscUtil.addCondition(lsSQL, "sBriefDsc LIKE " + SQLUtil.toSQL(fsValue + "%")); 
+                lsSQL += " LIMIT 1";
+            }
+        }
+        
+        ResultSet loRS = p_oApp.executeQuery(lsSQL);
+        
+        if (!loRS.next()){
+            MiscUtil.close(loRS);
+            p_sMessage = "No transaction found for the givern criteria.";
+            return false;
+        }
+        
+        lsSQL = loRS.getString("sTransNox");
+        MiscUtil.close(loRS);
+        
+        return OpenTransaction(lsSQL);
+    }
+    
+    public boolean OpenTransaction(String fsTransNox) throws SQLException{
+        p_nEditMode = EditMode.UNKNOWN;
+        
+        if (p_oApp == null){
+            p_sMessage = "Application driver is not set.";
+            return false;
+        }
+        
+        
+        p_sMessage = "";
+        String lsSQL;
+        ResultSet loRS;
+        RowSetFactory factory = RowSetProvider.newFactory();
+        
+        lsSQL = getSQ_Detail()+ " WHERE a.sTransNox = " + SQLUtil.toSQL(fsTransNox);
+        
+        //open master
+        loRS = p_oApp.executeQuery(lsSQL);
+        p_oMaster = factory.createCachedRowSet();
+        p_oMaster.populate(loRS);
+        MiscUtil.close(loRS);
+        
+        p_oMaster.last();
+        if (p_oMaster.getRow() <= 0) {
+            p_sMessage = "No transaction was loaded.";
+            return false;
+        }
+        p_nEditMode = EditMode.READY;
+        
+        return true;
+    }
+    
+    public boolean UpdateTransaction() throws SQLException{
+        if (p_nEditMode != EditMode.READY){
+            p_sMessage = "Invalid edit mode.";
+            return false;
+        }
+        
+        if (p_bWithParent) {
+            p_sMessage = "Updating of record from other object is not allowed.";
+            return false;
+        }
+//        
+        p_nEditMode = EditMode.UPDATE;
+        return true;
+    }
     
 }
---
+++ test/FAQuestionsTest.java
@@ -0,0 +1,111 @@
+
+import java.sql.SQLException;
+import org.json.simple.parser.ParseException;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.rmj.appdriver.GRider;
+import org.rmj.marketplace.base.LTransaction;
+import org.rmj.marketplace.base.ProductListing;
+import org.rmj.marketplace.base.FAQuestions;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class FAQuestionsTest {
+    static GRider instance = new GRider();
+    static FAQuestions trans;
+    static LTransaction listener;
+    
+    public FAQuestionsTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {        
+        if (!instance.logUser("gRider", "M001111122")){
+            System.err.println(instance.getMessage() + instance.getErrMsg());
+            System.exit(1);
+        }
+        
+        listener = new LTransaction() {
+            @Override
+            public void MasterRetreive(int fnIndex, Object foValue) {
+                System.out.println(fnIndex + "-->" + foValue);
+            }
+
+        };
+        trans = new FAQuestions(instance, instance.getBranchCode(), false);
+        trans.setListener(listener);
+        trans.setTranStat(12340);
+        trans.setWithUI(true);
+        
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Test
+    public void test01List() {
+        try {
+            if (trans.LoadList("", false)){//true if by barcode; false if by description
+                for (int lnCtr = 1; lnCtr <= trans.getItemCount(); lnCtr++){
+                    System.out.println("No.: " + lnCtr);
+                    System.out.println("Listing ID: " + (String) trans.getDetail(lnCtr,"sListngID"));
+                    System.out.println("Question : " + (String) trans.getDetail(lnCtr,"sQuestion"));
+                    System.out.println("Customer Name: " + trans.getDetail(lnCtr,"sCompnyNm"));
+                }
+            } else {
+                fail(trans.getMessage());
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        }
+    }
+    
+    @Test
+    public void test02OpenTransaction(){
+        try {
+            if (trans.OpenTransaction("C00122000002", "1")){
+                System.out.println("Listing ID: " + (String) trans.getMaster("sListngID"));
+                System.out.println("Question : " + (String) trans.getMaster("sQuestion"));
+                System.out.println("Customer Name: " + trans.getMaster("sCompnyNm"));
+                
+                if (trans.UpdateTransaction()){
+                    trans.setMaster("sReplyxxx", "Salamat po.");
+                    assertTrue(String.valueOf(trans.getMaster("sReplyxxx")).equalsIgnoreCase("Salamat po."));
+                } else {
+                    fail(trans.getMessage());
+                }
+            } else {
+                fail(trans.getMessage());
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        }
+    }
+    @Test
+    public void test03SaveTransaction() {
+        try {
+            if (trans.SaveTransaction()){
+                System.out.println("Reply to " + (String) trans.getMaster("sCompnyNm") + " has been sent.");
+            } else {
+                fail(trans.getMessage());
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        }
+        
+    }
+}
